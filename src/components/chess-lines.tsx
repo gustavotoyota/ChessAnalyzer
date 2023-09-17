6094f8cde2ff2ef01458ee8ac3dd2be72d8a3f66
Gustavo Toyota <gustavottoyota@gmail.com>
2023-09-17 10:22:49 -0400

    feat: stabilize chess lines height

--- src/components/chess-lines.tsx
+++ src/components/chess-lines.tsx
@@ -15,35 +15,42 @@ export default function ChessLines(props: {
     <>
       {[0, 1, 2, 3, 4]
         .map((i) => props.lines.get(i)!)
-        .filter((line) => line != null)
         .map((line, i) => (
           <div
             key={i}
             className="border-b border-neutral-400 py-1 overflow-hidden overflow-ellipsis whitespace-nowrap"
           >
-            <span
-              className="font-bold cursor-pointer"
-              onClick={() => props.onMovesSelected?.(line.moves.slice(0, 1))}
-            >
-              {line.scoreText}
-            </span>
-
-            <div className="inline-block w-2"></div>
-
-            {line.moves.map((move, i) => (
-              <span key={i}>
+            {line != null ? (
+              <>
                 <span
-                  className="cursor-pointer"
+                  className="font-bold cursor-pointer"
                   onClick={() =>
-                    props.onMovesSelected?.(line.moves.slice(0, i + 1))
+                    props.onMovesSelected?.(line.moves.slice(0, 1))
                   }
                 >
-                  {move.san}
+                  {line.scoreText}
                 </span>
 
-                <div className="inline-block w-1"></div>
-              </span>
-            ))}
+                <div className="inline-block w-2"></div>
+
+                {line.moves.map((move, i) => (
+                  <span key={i}>
+                    <span
+                      className="cursor-pointer"
+                      onClick={() =>
+                        props.onMovesSelected?.(line.moves.slice(0, i + 1))
+                      }
+                    >
+                      {move.san}
+                    </span>
+
+                    <div className="inline-block w-1"></div>
+                  </span>
+                ))}
+              </>
+            ) : (
+              <>&nbsp;</>
+            )}
           </div>
         ))}
     </>
