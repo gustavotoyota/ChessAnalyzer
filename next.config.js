c954711a75ea027a1d77d5554ed2f2d1e31bceac
Gustavo Toyota <gustavottoyota@gmail.com>
2025-04-27 14:26:15 -0300

    chore: export static

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
-        ],
-      },
-    ];
-  },
+  output: 'export',
 };
 
 module.exports = nextConfig;
